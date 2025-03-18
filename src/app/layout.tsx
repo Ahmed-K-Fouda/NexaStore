394c0f7b64c2e147744cc9a4b2bdf08b1cf2553b
Ahmed-K-Fouda <mido203966@gmail.com>
2025-03-19 01:02:05 +0200

    fix fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,13 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import type { Metadata } from "next";
 import "./globals.css";
-import { Inter } from "next/font/google";
 import Header from "./components/layout/Header";
 import { getCurrentSession } from "@/actions/auth";
 import { SanityLive } from "@/sanity/lib/live";
 import HeaderCategorySelector from "./components/layout/header_category/HeaderCategorySelector";
 import { getAllCategories } from "@/sanity/lib/client";
 import Cart from "./components/cart/Cart";
+import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
