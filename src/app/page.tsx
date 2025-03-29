e00aed680b91b9efd525896da8cdb7c6a39673f3
Ahmed-K-Fouda <mido203966@gmail.com>
2025-03-30 00:23:32 +0200

    pass props to wheelOfFortune component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,17 @@ import { getAllProducts } from "@/sanity/lib/client";
 import SalesCampaignBanner from "./components/layout/banner/SalesCampaignBanner";
 import ProductGrid from "./components/product/ProductGrid";
 import ToastProvider from './components/toastify/ToastProvider';
-import WheelOfFortune from "./components/layout/WheelOfFortune/WheelOfFortune";
+import WheelOfFortune from './components/layout/WheelOfFortune/WheelOfFortune';
+import { getWheelOfFortuneConfiguration } from './../actions/wheel-of-fortune-actions';
 export default async function Home() {
   const { user } = await getCurrentSession();
   const products = await getAllProducts();
+  const { randomProducts, winningIndex } = await getWheelOfFortuneConfiguration();
   return (
     <div>
       <ToastProvider/>
-      <WheelOfFortune/>
+      <WheelOfFortune  products={randomProducts}
+            winningIndex={winningIndex}/>
       <SalesCampaignBanner />
 
       <section className="container mx-auto px-8">
