d7dfcac399ac75fd16e6a76e4fc5819fbcafca90
Ahmed-K-Fouda <mido203966@gmail.com>
2025-04-03 18:04:21 +0200

    init umami

--- src/lib/umami.ts
+++ src/lib/umami.ts
@@ -1,12 +1,12 @@
-// import umami from "@umami/node";
+import umami from "@umami/node";
 
-// umami.init({
-//   websiteId: "(YOUR UMAMI WEBSITE ID)", // Your website id
-//   hostUrl: "https://cloud.umami.is", // URL to your Umami instance
-// });
+umami.init({
+  websiteId: "6d484aac-4a57-4b10-a935-3de37fedaafd", 
+  hostUrl: "https://cloud.umami.is", 
+});
 
-// export const umamiTrackCheckoutSuccessEvent = async (payload: {
-//   [key: string]: string | number | Date;
-// }) => {
-//   await umami.track("checkout_success", payload);
-// };
+export const umamiTrackCheckoutSuccessEvent = async (payload: {
+  [key: string]: string | number | Date;
+}) => {
+  await umami.track("checkout_success", payload);
+};
