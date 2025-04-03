f89cb58fff180a740d2f63b5eec2e2880aa37fc0
Ahmed-K-Fouda <mido203966@gmail.com>
2025-04-03 18:06:11 +0200

    add slider and banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,38 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { getCurrentSession } from "@/actions/auth";
 import { getAllProducts } from "@/sanity/lib/client";
-import SalesCampaignBanner from "./components/layout/banner/SalesCampaignBanner";
 import ProductGrid from "./components/product/ProductGrid";
 import ToastProvider from './components/toastify/ToastProvider';
 import WheelOfFortune from './components/layout/WheelOfFortune/WheelOfFortune';
 import { getWheelOfFortuneConfiguration } from './../actions/wheel-of-fortune-actions';
+import Banner from './components/product/Banner';
+import Slider from './components/product/Slider';
+import SalesCampaignBanner from './components/layout/banner/SalesCampaignBanner';
+import Footer from './components/footer/Footer';
+
 export default async function Home() {
   const { user } = await getCurrentSession();
-  const products = await getAllProducts();
+  const products = await getAllProducts(); 
   const { randomProducts, winningIndex } = await getWheelOfFortuneConfiguration();
+
   return (
     <div>
-      <ToastProvider/>
-      <WheelOfFortune  products={randomProducts}
-            winningIndex={winningIndex}/>
-      <SalesCampaignBanner />
+      <ToastProvider />
+      <WheelOfFortune products={randomProducts} winningIndex={winningIndex} />
+    <SalesCampaignBanner/>
+      <Banner products={products} />
 
-      <section className="container mx-auto px-8">
+      <section className="container mx-auto px-4">
+        <Slider products={products} />
+      </section>
+
+      <section className="container mx-auto px-4 mt-8">
+       <h2 className="text-3xl font-extrabold text-left mb-6 bg-gradient-to-r from-red-500 via-orange-500 to-yellow-400 text-transparent bg-clip-text drop-shadow-lg">
+        🛍️ All Products
+      </h2>
         <ProductGrid products={products} />
       </section>
+
+       <Footer/> 
     </div>
   );
 }
