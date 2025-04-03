addf82f4cafe04665937c8737bbf4ee0704d15f7
Ahmed-K-Fouda <mido203966@gmail.com>
2025-04-03 14:04:18 +0200

    make the time dynamic

--- src/app/components/layout/banner/SalesCampaignBanner.tsx
+++ src/app/components/layout/banner/SalesCampaignBanner.tsx
@@ -1,27 +1,43 @@
 "use client";
 import { useRouter } from "next/navigation";
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function SalesCampaignBanner() {
   const { push } = useRouter();
+
+  const initialTime = 24 * 60 * 60 - 1; 
+  const [timeLeft, setTimeLeft] = useState(initialTime);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimeLeft((prevTime) => (prevTime > 0 ? prevTime - 1 : 0));
+    }, 1000); 
+
+    return () => clearInterval(timer);
+  }, []);
+
+  const formatTime = (seconds: number) => {
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor((seconds % 3600) / 60);
+    const secs = seconds % 60;
+    return `${hours.toString().padStart(2, "0")}:${minutes
+      .toString()
+      .padStart(2, "0")}:${secs.toString().padStart(2, "0")}`;
+  };
+
   return (
-    <div className="w-full bg-gradient-to-r from-red-600  via-orange-500 to-red-600 py-3 relative overflow-hidden">
+    <div className="w-full bg-gradient-to-r from-red-600 via-orange-500 to-red-600 py-3 relative overflow-hidden">
       <div className="container mx-auto px-4">
         <div className="flex flex-col sm:flex-row items-center justify-center gap-2 sm:gap-6 text-white">
           <div className="flex items-center gap-2">
             <span className="text-xl sm:text-2xl font-bold animate-bounce">
-              {/* <img
-                className="sm:w-8 sm:h-8 w-6 h-6"
-                src="https://images.emojiterra.com/microsoft/fluent-emoji/15.1/512px/1f525_color.png"
-                alt="fire"
-              /> */}
               🔥
             </span>
             <div className="text-sm sm:text-base font-bold">
               FLASH SALE ENDS IN:
             </div>
             <div className="bg-white/20 px-2 py-1 font-mono font-bold rounded">
-              23:59:59
+              <span className='pulse animate_[pulse_infinite]'>{formatTime(timeLeft)}</span>
             </div>
           </div>
 
